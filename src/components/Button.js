12f724ef032a05a9711eb01dd84619739dd51ea5
Gaurav <gauravsarage.1902@gmail.com>
2023-06-24 18:29:02 +0530

    added minimalistic css designs

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,11 @@ import "../App.css";
 
 const Button = ({ text, setDisplay, display, equation, setEquation }) => {
     const handleClick = () => {
-
+        
     }
 
     return (
-        <button className="button"
-            onClick={handleClick}
-        >
+        <button className="button" onClick={handleClick}>
             {text}
         </button>
     );
